ecce509c96e18ca599b8958efc77dc30852793b8
John <126554267+J-v-Aby@users.noreply.github.com>
2023-11-20 14:19:04 +0100

    Update Modificari.cs

--- eserciziCorcoC.Net/esercizi_primo_modulo/Modificari.cs
+++ eserciziCorcoC.Net/esercizi_primo_modulo/Modificari.cs
@@ -18,20 +18,10 @@ namespace esercizi
         {
             return membroPrivato;           // Metodo pubblico che restituisce il membro privato
         }
-
-        
-       
-
         
         public double OttieniMembroInterno()
         {
             return membroInterno;           // Metodo pubblico che restituisce il membro interno
         }
-
-       /* // Metodo pubblico che restituisce il membro pubblico
-        public bool OttieniMembroPubblico()
-        {
-            return membroPubblico;
-        }*/
     }
 }
