6c321b2cc1adab68f387b51e3d13b43461a14db7
John <aby.vecchio@gmail.com>
2023-11-21 00:22:36 +0100

    ok

---
+++ eserciziCorcoC.Net/secondo_moduo/esercizioTreni/esercizioTreni/Program.cs
@@ -0,0 +1,22 @@
+﻿using esercizioTreni;
+
+LinkedList<Treno> listaTreni = new LinkedList<Treno>();
+
+listaTreni.AddLast(new Treno("Palermo"));
+listaTreni.AddLast(new Treno("Trapani"));
+listaTreni.AddLast(new Treno("Catania"));
+listaTreni.AddLast(new Treno("Messina"));
+listaTreni.AddFirst(new Treno("Roma"));
+
+    foreach (Treno t in listaTreni)
+    {
+        Console.WriteLine(t.nome);
+    }
+
+    listaTreni.RemoveFirst();
+Console.WriteLine("");
+foreach (Treno t in listaTreni)
+{
+    Console.WriteLine(t.nome);
+}
+
---
+++ eserciziCorcoC.Net/secondo_moduo/esercizioTreni/esercizioTreni/Treno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizioTreni
+{
+    internal class Treno
+    {
+        public string? nome {  get; set; }
+
+        public Treno (string? nome)
+        {
+            this.nome = nome;
+        }   
+    }
+}
