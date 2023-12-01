0cfab0f305fdf9b65f5e4fb5a077f271314a3f5e
John <aby.vecchio@gmail.com>
2023-12-01 17:47:49 +0100

    fixed

---
+++ eserciziCorcoC.Net/terzo_modulo/esercizio_that_sums_it_up/esercizio_that_sums_it_up/Avarege.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_that_sums_it_up
+{
+    internal class Avarege
+    {
+    }
+}
--- eserciziCorcoC.Net/terzo_modulo/terzo_modulo/Program.cs
+++ eserciziCorcoC.Net/terzo_modulo/terzo_modulo/Program.cs
@@ -10,14 +10,14 @@ List<int> list = Enumerable.Range(0, 1000).Select(list => random.Next(1001)).ToL
 //Il simbolo _ è spesso utilizzato quando il nome del parametro non è utilizzato ex: Select(_ => random.Next(1001)).ToList();
 
 var result = from item in list
-             where item % 3 == 0
+             where item % 2!= 0
              select item;
 
 Console.WriteLine(string.Join(" ", result));
 
                                                                  Console.WriteLine("\n secondo esercizio modulo 3\n");//sintax
 
-var result2 = list.Where(item => item % 3 == 0);
+var result2 = list.Where(item => item % 2!= 0);
 Console.WriteLine(string.Join(" ", result2));
 
                                                                    Console.WriteLine("\n terzo esercizio modulo 3\n");
@@ -35,7 +35,10 @@ Console.WriteLine("\nLista ordinata in modo Crescente:\n" + string.Join(" ", lon
 
 List<int> list4 = Enumerable.Range(0, 10).Select(l => random.Next(1001)).ToList();
 
-var numeroDiNumeri = list4.Select(item => $"{item} = {item.ToString().Length} carattere");
+var numeriordinati = list4.OrderBy(o => o).ToList();
+
+var numeroDiNumeri = numeriordinati.Select(item => $"{item} = {item.ToString().Length} carattere");
+
 
 Console.WriteLine(string.Join(" \n*", numeroDiNumeri));
 
