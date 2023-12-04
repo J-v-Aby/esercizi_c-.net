1b9909bb50c0374713985a59aecb8046185863bb
John <aby.vecchio@gmail.com>
2023-12-04 18:48:30 +0100

    quarto modulo

---
+++ eserciziCorcoC.Net/quarto_modulo/esercizio uno/esercizio uno/MyQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_uno
+{
+    public class MyQueue<T>
+{
+    private Queue<T> elemento;
+
+    public MyQueue()
+    {
+        elemento = new Queue<T>();
+    }
+
+    // Metodo per aggiungere un elemento alla coda
+    public void AddQ(T elemento)
+    {
+        this.elemento.Enqueue(elemento);
+    }
+
+    // Metodo per rimuovere e restituire l'elemento in testa alla coda
+    public T DeliteQ()
+    {
+        if (elemento.Count == 0)
+        {
+                Console.WriteLine("La coda è vuota");
+        }
+
+        return elemento.Dequeue();
+    }
+
+    // Metodo per sbirciare l'elemento in testa alla coda senza rimuoverlo
+    public T SeeQ()
+    {
+        if (elemento.Count == 0)
+        {
+                Console.WriteLine("La coda è vuota");
+        }
+
+        return elemento.Peek();
+    }
+
+}
+
+}
+
---
+++ eserciziCorcoC.Net/quarto_modulo/esercizio uno/esercizio uno/Program.cs
@@ -0,0 +1,16 @@
+﻿using esercizio_uno;
+using System;
+using System.Collections.Generic;
+
+
+MyQueue<int> codaInt=new MyQueue<int>();
+codaInt.AddQ(1);
+codaInt.AddQ(2);
+codaInt.AddQ(3);
+
+Console.WriteLine($"Elemento {codaInt.SeeQ()}"); 
+
+codaInt.DeliteQ();
+
+Console.WriteLine($"Elemento in testa alla coda dopo la rimozione: {codaInt.SeeQ()}"); 
+    
